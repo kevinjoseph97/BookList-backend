20c5fcc8610d8a8d24da7a8bc5cee1e124e6eb31
kevinjoseph97 <kevinjoseph97@gmail.com>
2021-07-18 19:08:29 -0400

    first commit

---
+++ db/seeds.rb
@@ -0,0 +1,47 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+Book.create([
+    {
+    title: " The Phantom Tollbooth",
+    author: "Norton Juster",
+    image: "https://www.powells.com/portals/0/images/9780394820378.jpg"
+   
+  },
+  {
+    title: "To Kill a Mockingbird",
+    author: " Harper Lee",
+    image: "https://www.powells.com/portals/0/images/9780061120084.jpg"
+  
+  },
+  {
+    title: "2666",
+    author: "Roberto Bolaño",
+    image: "https://www.powells.com/portals/0/images/9780312429218.jpg"
+  
+  },
+  {
+    title: " The Call of the Wild",
+    author: "Jack London",
+    image: "https://covers.powells.com/9780439227148.jpg"
+   
+  },
+  {
+    title: " All the Light We Cannot See",
+    author: " Anthony Doerr",
+    image: "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1451445646l/18143977.jpg"
+   
+  },
+  {
+    title: " The Book Thief",
+    author: " Markus Zusak",
+    image: "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1522157426l/19063._SY475_.jpg"
+   
+  },
+  
+])
